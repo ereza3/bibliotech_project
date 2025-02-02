d846877bbac77865120dde0675a06d5ac56e2b79
ereza3 <er69936@ubt-uni.net>
2025-02-02 23:27:12 +0100

    Create footer.php

---
+++ View/includes/footer.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+    <div class="footer-content">
+      <div class="footer-section about">
+        <h3>About Us</h3>
+        <p>In a world full of technology, get your e-book in a fast way. We are your solution.</p>
+      </div>
+      <div class="footer-section links">
+        <h3>Quick Links</h3>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#services">About Us</a></li>
+          <li><a href="#contact">Books</a></li>
+          <li><a href="#blog">Contact</a></li>
+        </ul>
+      </div>
+      <div class="footer-section social">
+        <h3>Follow Us</h3>
+        <div class="social-links">
+          <a href="#" class="social-icon"><i class="fab fa-facebook"></i></a>
+          <a href="#" class="social-icon"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p>&copy; 2024 Bibliotech | All Rights Reserved</p>
+    </div>
+  </footer>
